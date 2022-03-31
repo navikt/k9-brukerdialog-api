787d91b58cd18944a500683ddae2743928128a4f
Sander Opperud Odden <sander.opperud.odden@nav.no>
2022-03-31 12:56:48 +0200

    .

--- src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/omsorgspengermidlertidigalene/OmsorgspengerMidlertidigAleneApis.kt
+++ src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/omsorgspengermidlertidigalene/OmsorgspengerMidlertidigAleneApis.kt
@@ -13,6 +13,7 @@ import no.nav.k9brukerdialogapi.general.getCallId
 import no.nav.k9brukerdialogapi.kafka.getMetadata
 import no.nav.k9brukerdialogapi.ytelse.Ytelse
 import no.nav.k9brukerdialogapi.ytelse.omsorgspengermidlertidigalene.domene.Søknad
+import no.nav.k9brukerdialogapi.ytelse.registrerMottattSøknad
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -27,6 +28,7 @@ fun Route.omsorgspengerMidlertidigAleneApis(
             val søknad =  call.receive<Søknad>()
             logger.info(formaterStatuslogging(Ytelse.OMSORGSPENGER_MIDLERTIDIG_ALENE, søknad.søknadId, "mottatt."))
             omsorgspengerMidlertidigAleneService.registrer(søknad, call.getCallId(), call.getMetadata(), idTokenProvider.getIdToken(call))
+            registrerMottattSøknad(Ytelse.OMSORGSPENGER_MIDLERTIDIG_ALENE)
             call.respond(HttpStatusCode.Accepted)
         }
     }
--- src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/omsorgspengerutvidetrett/OmsorgspengerUtvidetRettApis.kt
+++ src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/omsorgspengerutvidetrett/OmsorgspengerUtvidetRettApis.kt
@@ -13,6 +13,7 @@ import no.nav.k9brukerdialogapi.general.getCallId
 import no.nav.k9brukerdialogapi.kafka.getMetadata
 import no.nav.k9brukerdialogapi.ytelse.Ytelse
 import no.nav.k9brukerdialogapi.ytelse.omsorgspengerutvidetrett.domene.Søknad
+import no.nav.k9brukerdialogapi.ytelse.registrerMottattSøknad
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -27,6 +28,7 @@ fun Route.omsorgspengerUtvidetRettApis(
             val søknad =  call.receive<Søknad>()
             logger.info(formaterStatuslogging(Ytelse.OMSORGSPENGER_UTVIDET_RETT, søknad.søknadId, "mottatt."))
             omsorgspengerUtvidetRettService.registrer(søknad, call.getCallId(), call.getMetadata(), idTokenProvider.getIdToken(call))
+            registrerMottattSøknad(Ytelse.OMSORGSPENGER_UTVIDET_RETT)
             call.respond(HttpStatusCode.Accepted)
         }
     }
