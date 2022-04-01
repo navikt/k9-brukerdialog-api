a58dc21cc745d1b0ba462d35a10fe388a191750e
Sander Opperud Odden <sander.opperud.odden@nav.no>
2022-04-01 10:55:51 +0200

    Legger til custom counter som teller antall mottatte søknader per ytelse

--- src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/ettersending/EttersendingApis.kt
+++ src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/ettersending/EttersendingApis.kt
@@ -11,8 +11,9 @@ import no.nav.k9brukerdialogapi.INNSENDING_URL
 import no.nav.k9brukerdialogapi.general.formaterStatuslogging
 import no.nav.k9brukerdialogapi.general.getCallId
 import no.nav.k9brukerdialogapi.kafka.getMetadata
-import no.nav.k9brukerdialogapi.ytelse.Ytelse
+import no.nav.k9brukerdialogapi.ytelse.Ytelse.ETTERSENDING
 import no.nav.k9brukerdialogapi.ytelse.ettersending.domene.Søknad
+import no.nav.k9brukerdialogapi.ytelse.registrerMottattSøknad
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -25,8 +26,9 @@ fun Route.ettersendingApis(
     route(ETTERSENDING_URL){
         post(INNSENDING_URL){
             val søknad =  call.receive<Søknad>()
-            logger.info(formaterStatuslogging(Ytelse.ETTERSENDING, søknad.søknadId, "mottatt."))
+            logger.info(formaterStatuslogging(ETTERSENDING, søknad.søknadId, "mottatt."))
             ettersendingService.registrer(søknad, call.getCallId(), call.getMetadata(), idTokenProvider.getIdToken(call))
+            registrerMottattSøknad(ETTERSENDING)
             call.respond(HttpStatusCode.Accepted)
         }
     }
