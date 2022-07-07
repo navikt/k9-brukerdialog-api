1cb71c0a3b443d48129568192eeaf821fbf66702
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2022-07-07 13:21:55 +0200

    Legger til Fordele og tilhørende enhetstester. (#98)
    * Legger til Fordele og tilhørende enhetstester.
    
    * Oppdatere pakke.
    * Closes #96

---
+++ src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/omsorgsdagermelding/domene/Fordele.kt
@@ -0,0 +1,25 @@
+package no.nav.k9brukerdialogapi.ytelse.omsorgsdagermelding.domene
+
+import no.nav.k9brukerdialogapi.general.krever
+import no.nav.k9brukerdialogapi.vedlegg.vedleggId
+import no.nav.k9brukerdialogapi.ytelse.omsorgsdagermelding.domene.Mottaker.SAMVÆRSFORELDER
+import java.net.URL
+
+class Fordele(
+    private val mottakerType: Mottaker,
+    private val samværsavtale: List<URL> = listOf()
+){
+    internal fun valider(felt: String) = mutableListOf<String>().apply {
+        krever(mottakerType == SAMVÆRSFORELDER, "$felt.mottakerType må være '$SAMVÆRSFORELDER'.")
+    }
+
+    internal fun somKomplettFordele() = KomplettFordele(
+        mottakerType = mottakerType,
+        samværsavtaleVedleggId = samværsavtale.map { url -> url.vedleggId() }
+    )
+}
+
+class KomplettFordele(
+    private val mottakerType: Mottaker,
+    internal val samværsavtaleVedleggId: List<String>
+)
