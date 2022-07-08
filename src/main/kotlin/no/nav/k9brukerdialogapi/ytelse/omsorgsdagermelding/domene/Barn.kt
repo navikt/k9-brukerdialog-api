eb2d0caf632840a5485c9cfb672c4522ceb73875
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2022-07-08 10:13:38 +0200

    Legger til Barn for melding om dele omsorgsdager. (#100)
    * Legger til Barn for melding om dele omsorgsdager.
    
    * * Closes #99
    
    * Legger til sjekk slik at man ikke ved en feil oppdaterer fnr på barn som allerede har.

---
+++ src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/omsorgsdagermelding/domene/Barn.kt
@@ -0,0 +1,29 @@
+package no.nav.k9brukerdialogapi.ytelse.omsorgsdagermelding.domene
+
+import no.nav.k9brukerdialogapi.general.krever
+import no.nav.k9brukerdialogapi.general.kreverIkkeNull
+import no.nav.k9brukerdialogapi.general.validerIdentifikator
+import java.time.LocalDate
+
+class Barn(
+    internal val aktørId: String? = null,
+    private var identitetsnummer: String? = null,
+    private val fødselsdato: LocalDate,
+    private val navn: String,
+    private val aleneOmOmsorgen: Boolean? = null,
+    private val utvidetRett: Boolean? = null
+) {
+    fun valider(felt: String) = mutableListOf<String>().apply {
+        validerIdentifikator(identitetsnummer, "$felt.identitetsnummer")
+        krever(navn.isNotBlank(), "$felt.navn kan ikke være tomt eller blank.")
+        kreverIkkeNull(aleneOmOmsorgen, "$felt.aleneOmOmsorgen kan ikke være null. Må være true/false.")
+        kreverIkkeNull(utvidetRett, "$felt.utvidetRett kan ikke være null. Må være true/false.")
+    }
+
+    fun manglerIdentitetsnummer() = identitetsnummer.isNullOrBlank()
+    fun oppdaterIdentitetsnummerMed(identitetsnummer: String){
+        require(manglerIdentitetsnummer()) { "Kan kun oppdatere identitetsnummer på barn som mangler." }
+        this.identitetsnummer = identitetsnummer
+    }
+
+}
