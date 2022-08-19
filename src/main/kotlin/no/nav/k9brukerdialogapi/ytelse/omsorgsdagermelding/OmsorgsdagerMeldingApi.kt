33a4ad24ee5b531c880f2d208fe88e3553e29768
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2022-08-19 14:50:52 +0200

    Registrerer mottatt melding metrikk (#116)
    * Registrerer mottatt melding metrikk

--- src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/omsorgsdagermelding/OmsorgsdagerMeldingApi.kt
+++ src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/omsorgsdagermelding/OmsorgsdagerMeldingApi.kt
@@ -14,7 +14,10 @@ import no.nav.k9brukerdialogapi.OMSORGSDAGER_MELDING_OVERFORING_URL
 import no.nav.k9brukerdialogapi.general.formaterStatuslogging
 import no.nav.k9brukerdialogapi.general.getCallId
 import no.nav.k9brukerdialogapi.kafka.getMetadata
+import no.nav.k9brukerdialogapi.ytelse.Ytelse
+import no.nav.k9brukerdialogapi.ytelse.Ytelse.*
 import no.nav.k9brukerdialogapi.ytelse.omsorgsdagermelding.domene.Melding
+import no.nav.k9brukerdialogapi.ytelse.registrerMottattSøknad
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -25,24 +28,26 @@ fun Route.omsorgsdagerMeldingApi(
     omsorgsdagerMeldingService: OmsorgsdagerMeldingService
 ) {
     post(OMSORGSDAGER_MELDING_FORDELING_URL+INNSENDING_URL){
-        mottaMelding(omsorgsdagerMeldingService, idTokenProvider)
+        mottaMelding(omsorgsdagerMeldingService, idTokenProvider, OMSORGSDAGER_MELDING_FORDELING)
     }
 
     post(OMSORGSDAGER_MELDING_OVERFORING_URL+INNSENDING_URL){
-        mottaMelding(omsorgsdagerMeldingService, idTokenProvider)
+        mottaMelding(omsorgsdagerMeldingService, idTokenProvider, OMSORGSDAGER_MELDING_OVERFORING)
     }
 
     post(OMSORGSDAGER_MELDING_KORONAOVERFORING_URL+INNSENDING_URL){
-        mottaMelding(omsorgsdagerMeldingService, idTokenProvider)
+        mottaMelding(omsorgsdagerMeldingService, idTokenProvider, OMSORGSDAGER_MELDING_KORONAOVERFORING)
     }
 }
 
 private suspend fun PipelineContext<Unit, ApplicationCall>.mottaMelding(
     omsorgsdagerMeldingService: OmsorgsdagerMeldingService,
-    idTokenProvider: IdTokenProvider
+    idTokenProvider: IdTokenProvider,
+    ytelse: Ytelse
 ) {
     val melding = call.receive<Melding>()
     logger.info(formaterStatuslogging(melding.type.somYtelse(), melding.søknadId, "mottatt."))
     omsorgsdagerMeldingService.registrer(melding, call.getCallId(), call.getMetadata(), idTokenProvider.getIdToken(call))
+    registrerMottattSøknad(ytelse)
     call.respond(HttpStatusCode.Accepted)
 }
