3abc48952e72dae43ed444782cbede4a1c50e93a
Sander Opperud Odden <sander.opperud.odden@nav.no>
2022-09-09 10:33:40 +0200

    Legger til funksjonalitet for SelvstendigNæringsdrivende

--- src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/pleiepengerlivetssluttfase/domene/SelvstendigNæringsdrivende.kt
+++ src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/pleiepengerlivetssluttfase/domene/SelvstendigNæringsdrivende.kt
@@ -1,5 +1,8 @@
 package no.nav.k9brukerdialogapi.ytelse.pleiepengerlivetssluttfase.domene
 
+import no.nav.k9brukerdialogapi.ytelse.pleiepengerlivetssluttfase.domene.Arbeidsforhold.Companion.somK9ArbeidstidInfo
+import java.time.LocalDate
+
 class SelvstendigNæringsdrivende(
     val virksomhet: no.nav.k9brukerdialogapi.ytelse.omsorgspengerutbetalingsnf.domene.SelvstendigNæringsdrivende,
     val arbeidsforhold: Arbeidsforhold
@@ -8,4 +11,7 @@ class SelvstendigNæringsdrivende(
         addAll(virksomhet.valider("$felt.virksomhet"))
         addAll(arbeidsforhold.valider("$felt.arbeidsforhold"))
     }
+
+    fun somK9SelvstendigNæringsdrivende() = virksomhet.somK9SelvstendigNæringsdrivende()
+    fun somK9ArbeidstidInfo(fraOgMed: LocalDate, tilOgMed: LocalDate) = arbeidsforhold.somK9ArbeidstidInfo(fraOgMed, tilOgMed)
 }
