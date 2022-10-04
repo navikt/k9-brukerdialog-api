dc1df5c6d0ddb59f55b9a102ceb9c7ac1d2f9975
albrektsson <emil.albrektsson@nav.no>
2022-10-04 10:13:24 +0200

    Pils validator..

--- src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/pleiepengerlivetssluttfase/K9FormatValidering.kt
+++ src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/pleiepengerlivetssluttfase/K9FormatValidering.kt
@@ -4,19 +4,22 @@ import no.nav.helse.dusseldorf.ktor.core.ParameterType
 import no.nav.helse.dusseldorf.ktor.core.Throwblem
 import no.nav.helse.dusseldorf.ktor.core.ValidationProblemDetails
 import no.nav.helse.dusseldorf.ktor.core.Violation
+import no.nav.k9.søknad.Søknad
 import no.nav.k9.søknad.ytelse.pls.v1.PleiepengerLivetsSluttfaseSøknadValidator
 
-fun validerK9FormatPILS(k9Format: no.nav.k9.søknad.Søknad) {
-    val mangler = PleiepengerLivetsSluttfaseSøknadValidator().valider(k9Format).map {
-        Violation(
-            parameterName = it.felt,
-            parameterType = ParameterType.ENTITY,
-            reason = it.feilmelding,
-            invalidValue = "K9-format feilkode: ${it.feilkode}"
-        )
-    }.toMutableSet()
+fun validerK9FormatPILS(søknad: Søknad) {
+    val valideringsfeil = PleiepengerLivetsSluttfaseSøknadValidator()
+        .valider(søknad)
+        .map {
+            Violation(
+                parameterName = it.felt,
+                parameterType = ParameterType.ENTITY,
+                reason = it.feilmelding,
+                invalidValue = "K9-format feilkode: ${it.feilkode}"
+            )
+        }.toMutableSet()
 
-    if (mangler.isNotEmpty()) {
-        throw Throwblem(ValidationProblemDetails(mangler))
+    if (valideringsfeil.isNotEmpty()) {
+        throw Throwblem(ValidationProblemDetails(valideringsfeil))
     }
 }
