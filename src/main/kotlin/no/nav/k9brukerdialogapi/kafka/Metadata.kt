75c1ac50aca281f8efbdbc048100a8264c1144d0
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-02-28 08:35:31 +0100

    Debugger header
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/k9brukerdialogapi/kafka/Metadata.kt
+++ src/main/kotlin/no/nav/k9brukerdialogapi/kafka/Metadata.kt
@@ -3,6 +3,7 @@ package no.nav.k9brukerdialogapi.kafka
 import io.ktor.server.application.ApplicationCall
 import io.ktor.server.request.header
 import no.nav.k9brukerdialogapi.general.getCallId
+import org.slf4j.LoggerFactory
 
 data class Metadata(
     val version: Int,
@@ -10,8 +11,13 @@ data class Metadata(
     val soknadDialogCommitSha: String? = null
 )
 
-fun ApplicationCall.getMetadata() = Metadata(
-    version = 1,
-    correlationId = getCallId().value,
-    soknadDialogCommitSha = this.request.header("X-Brukerdialog-Git-Sha")
-)
+fun ApplicationCall.getMetadata(): Metadata {
+    val header = this.request.header("X-Brukerdialog-Git-Sha")
+    val logger = LoggerFactory.getLogger("ApplicationCall.getMetadata()")
+    logger.info("X-Brukerdialog-Git-Sha = $header")
+    return Metadata(
+        version = 1,
+        correlationId = getCallId().value,
+        soknadDialogCommitSha = header
+    )
+}
