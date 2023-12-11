217bf0fee07abd1a9392cfa1a01fd62add3ce698
Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>
2023-12-11 16:48:48 +0100

    Legger til info om høyere risiko for fravær fra jobb.
    Legger til regex mønster for fritekstfelt.
    
    Signed-off-by: Ramin Esfandiari <ramin_esfandiari_93@hotmail.com>

--- src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/omsorgspengerutvidetrett/domene/OmsorgspengerKroniskSyktBarnSøknad.kt
+++ src/main/kotlin/no/nav/k9brukerdialogapi/ytelse/omsorgspengerutvidetrett/domene/OmsorgspengerKroniskSyktBarnSøknad.kt
@@ -15,6 +15,8 @@ import no.nav.k9brukerdialogapi.innsending.Innsending
 import no.nav.k9brukerdialogapi.kafka.Metadata
 import no.nav.k9brukerdialogapi.oppslag.barn.BarnOppslag
 import no.nav.k9brukerdialogapi.oppslag.søker.Søker
+import no.nav.k9brukerdialogapi.utils.StringUtils.FRITEKST_REGEX
+import no.nav.k9brukerdialogapi.utils.StringUtils.FritekstPattern
 import no.nav.k9brukerdialogapi.vedlegg.vedleggId
 import no.nav.k9brukerdialogapi.ytelse.Ytelse
 import no.nav.k9brukerdialogapi.ytelse.fellesdomene.Barn
@@ -29,15 +31,17 @@ class OmsorgspengerKroniskSyktBarnSøknad(
     private val mottatt: ZonedDateTime = ZonedDateTime.now(ZoneOffset.UTC),
     private val språk: String,
     private var barn: Barn,
-    private val sammeAdresse: Boolean?,
     internal val legeerklæring: List<URL> = listOf(),
     internal val samværsavtale: List<URL>? = null,
     private val relasjonTilBarnet: SøkerBarnRelasjon? = null,
     private val kroniskEllerFunksjonshemming: Boolean,
     private val harForståttRettigheterOgPlikter: Boolean,
     private val harBekreftetOpplysninger: Boolean,
-    private val dataBruktTilUtledningAnnetData: String? = null
-): Innsending {
+    private val sammeAdresse: BarnSammeAdresse?,
+    private val høyereRisikoForFravær: Boolean? = null, // TODO: Fjern nullable når lansert
+    private val høyereRisikoForFraværBeskrivelse: String? = null, // skal valideres hvis høyereRisikoForFravær er true
+    private val dataBruktTilUtledningAnnetData: String? = null,
+) : Innsending {
 
     companion object {
         private val k9FormatVersjon = Versjon.of("1.0.0")
@@ -67,7 +71,11 @@ class OmsorgspengerKroniskSyktBarnSøknad(
         .medSoknadDialogCommitSha(metadata.soknadDialogCommitSha)
         .medAnnetData(dataBruktTilUtledningAnnetData)
 
-    override fun somKomplettSøknad(søker: Søker, k9Format: no.nav.k9.søknad.Innsending?, titler: List<String>): OmsorgspengerKroniskSyktBarnKomplettSøknad {
+    override fun somKomplettSøknad(
+        søker: Søker,
+        k9Format: no.nav.k9.søknad.Innsending?,
+        titler: List<String>,
+    ): OmsorgspengerKroniskSyktBarnKomplettSøknad {
         requireNotNull(k9Format)
         return OmsorgspengerKroniskSyktBarnKomplettSøknad(
             språk = språk,
@@ -78,6 +86,8 @@ class OmsorgspengerKroniskSyktBarnSøknad(
             barn = barn,
             relasjonTilBarnet = relasjonTilBarnet,
             sammeAdresse = sammeAdresse,
+            høyereRisikoForFravær = høyereRisikoForFravær,
+            høyereRisikoForFraværBeskrivelse = høyereRisikoForFraværBeskrivelse,
             legeerklæringVedleggId = legeerklæring.map { it.vedleggId() },
             samværsavtaleVedleggId = samværsavtale?.map { it.vedleggId() } ?: listOf(),
             harForståttRettigheterOgPlikter = harForståttRettigheterOgPlikter,
@@ -90,15 +100,36 @@ class OmsorgspengerKroniskSyktBarnSøknad(
         krever(harBekreftetOpplysninger, "harBekreftetOpplysninger må være true")
         krever(harForståttRettigheterOgPlikter, "harForståttRettigheterOgPlikter må være true")
         kreverIkkeNull(sammeAdresse, "sammeAdresse må være satt.")
-        if(sammeAdresse != null && !sammeAdresse){
-            krever(samværsavtale?.isNotEmpty(), "Dersom sammeAdresse er false kan ikke samværsavtale være tom.")
+
+        if (sammeAdresse != null && sammeAdresse == BarnSammeAdresse.NEI) {
+            krever(samværsavtale?.isNotEmpty(), "Dersom sammeAdresse er NEI kan ikke samværsavtale være tom.")
         }
+
+        if (høyereRisikoForFravær == true) {
+            krever(
+                !høyereRisikoForFraværBeskrivelse.isNullOrBlank(),
+                "høyereRisikoForFraværBeskrivelse må være satt når høyereRisikoForFravær er true"
+            )
+            if (!høyereRisikoForFraværBeskrivelse.isNullOrBlank()) {
+                krever(
+                    høyereRisikoForFraværBeskrivelse.length in 1..1000,
+                    "høyereRisikoForFraværBeskrivelse må være mellom 1 og 1000 tegn"
+                )
+                krever(
+                    FRITEKST_REGEX.matches(høyereRisikoForFraværBeskrivelse),
+                    "høyereRisikoForFraværBeskrivelse matcher ikke tilatt møønster: $FritekstPattern"
+                )
+            }
+        }
+
         addAll(barn.valider("barn"))
 
         if (isNotEmpty()) throw Throwblem(ValidationProblemDetails(this))
     }
 
-    override fun søknadValidator(): SøknadValidator<no.nav.k9.søknad.Søknad> = OmsorgspengerKroniskSyktBarnSøknadValidator()
+    override fun søknadValidator(): SøknadValidator<no.nav.k9.søknad.Søknad> =
+        OmsorgspengerKroniskSyktBarnSøknadValidator()
+
     override fun ytelse(): Ytelse = Ytelse.OMSORGSPENGER_UTVIDET_RETT
     override fun søknadId(): String = søknadId
     override fun vedlegg(): List<URL> = mutableListOf<URL>().apply {
